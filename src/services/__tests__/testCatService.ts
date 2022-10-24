37f4cfafe98acce965147b5b9543ef977fb24642
Dumbleedore <lucas.pari@icloud.com>
2022-10-24 15:46:33 -0300

    updates

--- src/services/__tests__/testCatService.ts
+++ src/services/__tests__/testCatService.ts
@@ -1,5 +1,11 @@
-import { fetchCat } from "../catService";
+import { fetchCat, fetchCatList } from "../catService";
 it("returns a cat", async () => {
   const response = await fetchCat();
   expect(response.length).toBe(1);
+});
+
+// test that cat service return 9 cats
+it("returns 9 cats", async () => {
+  const response = await fetchCatList();
+  expect(response.length).toBe(9);
 });
